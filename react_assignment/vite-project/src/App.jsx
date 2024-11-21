97443e45c18bce1334ccc79fe010f4b0b44f8d2b
raj9825 <rajparmar2281@gmail.com>
2024-11-21 15:50:58 +0530

    done

---
+++ react_assignment/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <span>React JS</span>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
